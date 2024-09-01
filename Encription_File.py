ed1f120284bb52adac4fafae6f2544a1f3db3706
peekaboo2803 <kirtanskh@gmail.com>
2024-08-31 22:23:25 -0400

    Starting over the whole code

--- Encription_File.py
+++ Encription_File.py
@@ -1,2 +1,21 @@
 print('This is an Encription file')
+'''#from Main import InputKey
 
+#open File where the keystorkes will be recorded
+Storage = open("Keyboard_Save.txt", "a")
+#Storage.write(f'{InputKey}')
+def letterA(x):
+    if x == 'A':
+        x = 'e'
+        Storage.write(InputKey)
+    else:
+        pass
+
+
+def printing():
+    Storage.write("Function is running")
+    print("This fUNCTION is running")
+
+
+
+Storage.close() #Closing file'''
--- Main.py
+++ Main.py
@@ -1,31 +1,47 @@
-print('This is the main file')
+#print('This is the main file')
 
+'''from Encription_File import *
 from pynput import keyboard
 #from pynput.keyboard import Listener
 #from pynput.mouse import Listener
 
-'''def keyboard_clicks(key):
-    print(f"{key} pressed")
-
-keyboard_clicks'''
-#open File where the keystorkes will be recorded
-Storage = open("Keyboard_Save.txt", "a")
-
 from pynput.keyboard import Listener
+
+#Storage = open("Keyboard_Save.txt", "a")
 
 #Fucntion with application of recording keystokes
 def on_press(key):
-    Storage.write(f'{key}')
-    print(f'{key} pressed')
+    global InputKey
+    InputKey = key
+    #letterA(InputKey)
+    #Storage.write(f'{InputKey}')
+    print(f'{InputKey} pressed')
+    printing()
     if key == keyboard.Key.esc:
         return False  
         # Stop listener
 
-'''def on_release(key):
-    print(f'{key} released')'''
 
 
 with Listener(on_press=on_press) as listener:
     listener.join()
 
-Storage.close() #Closing file
+#Storage.close() #Closing file'''
+
+#Start from scratch
+
+from pynput import keyboard
+from pynput.keyboard import Listener
+
+def record_click(key):
+    try:
+        print(key.char, end='')
+    except AttributeError:
+        print(f'[{key}]', end='')
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False  # Stop listener
+
+with keyboard.Listener(on_press=record_click, on_release=on_release) as listener:
+    listener.join()
