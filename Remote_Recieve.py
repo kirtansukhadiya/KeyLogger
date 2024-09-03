c36362ef5949bdcaa2d5c6139d3372d39b89e704
peekaboo2803 <kirtanskh@gmail.com>
2024-09-03 16:54:28 -0400

    added remote sending feature so that data can be recieved on a remote computer

---
+++ Remote_Recieve.py
@@ -0,0 +1,23 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_socket.bind(('localhost', 13579))
+
+server_socket.listen(1)
+
+print("Waiting for connection...")
+
+acc, addr = server_socket.accept()
+print("Connected to {addr}")
+
+with open('received_file.txt', 'wb') as f:
+    while True:
+        # Receive data in chunks
+        data = acc.recv(1024)
+        if not data:
+            break  # Exit loop if no more data is received
+        f.write(data)
+
+print("File received successfully.")
+acc.close()
